a4e32429f994eb7b168696857d0446c0dff80b39
Miguel Lopes Isidoro <miguel.isidoro@create.pt>
2024-07-10 16:52:51 +0100

    Dev

--- app/routes/contacts.$contactId_.edit.tsx
+++ app/routes/contacts.$contactId_.edit.tsx
@@ -1,5 +1,4 @@
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
 import { getContact, updateContact } from "../data";
