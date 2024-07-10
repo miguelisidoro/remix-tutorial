06549c98dfe9ae860a8798f355fb2892455eaed3
Miguel Lopes Isidoro <miguel.isidoro@create.pt>
2024-07-10 11:45:41 +0100

    Dev

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,31 +1,31 @@
-import { Form } from "@remix-run/react";
 import type { FunctionComponent } from "react";
+import { json } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import { getContact } from "../data";
 
 import type { ContactRecord } from "../data";
 
+export const loader = async ({ params }) => {
+  const contact = await getContact(params.contactId);
+  return json({ contact });
+};
+
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placekitten.com/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+  const { contact } = useLoaderData<typeof loader>();
 
   return (
     <div id="contact">
       <div>
         <img
-          alt={`${contact.first} ${contact.last} avatar`}
-          key={contact.avatar}
-          src={contact.avatar}
+          alt={`${contact?.first} ${contact?.last} avatar`}
+          key={contact?.avatar}
+          src={contact?.avatar}
         />
       </div>
 
       <div>
         <h1>
-          {contact.first || contact.last ? (
+          {contact?.first || contact?.last ? (
             <>
               {contact.first} {contact.last}
             </>
@@ -35,7 +35,7 @@ export default function Contact() {
           <Favorite contact={contact} />
         </h1>
 
-        {contact.twitter ? (
+        {contact?.twitter ? (
           <p>
             <a
               href={`https://twitter.com/${contact.twitter}`}
@@ -45,7 +45,7 @@ export default function Contact() {
           </p>
         ) : null}
 
-        {contact.notes ? <p>{contact.notes}</p> : null}
+        {contact?.notes ? <p>{contact.notes}</p> : null}
 
         <div>
           <Form action="edit">
